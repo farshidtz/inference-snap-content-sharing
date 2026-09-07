fd48aa731dfaeb150df9aa56e683490cd89e8a8e
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-09-07 15:45:22 +0200

    Add provider subdirectory to consumer

--- inference-snap-to-clients/gemma3-jane/app.sh
+++ inference-snap-to-clients/gemma3-jane/app.sh
@@ -4,3 +4,4 @@ dir=$SNAP_COMMON/share/provider/gemma3-jane
 mkdir -p $dir
 echo "# $(date)" > $dir/provider.env
 echo "OPENAI_BASE_URL=http://localhost:8080/v1" >> $dir/provider.env
+echo "SNAP_NAME=$SNAP_NAME" >> $dir/provider.env
